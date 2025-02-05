fb12d5adf45c6c7d44355e75ca8c42d98289f235
Jim Toth <jim@artby.city>
2025-02-05 06:37:12 -0500

    Reduce debug log spam

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
-    logger: ['error', 'warn', 'log']//, 'debug'] // 'verbose']
+    logger: ['error', 'warn', 'log', 'debug'] // 'verbose']
   })
   await app.listen(process.env.PORT ?? 3000)
 }
